82084100b285dbf653662e0a1d7ea0e20a38a8fc
MarlonVCendron <marlonvcendron@gmail.com>
2021-06-11 18:25:41 -0300

    Adicionados campos estoque e estante produto

--- src/entities/Product.php
+++ src/entities/Product.php
@@ -1,44 +1,64 @@
 <?php
+
+namespace App\entities;
+
 class Product {
   private $id;
   private $name;
   private $price;
   private $stock;
+  private $provider;
+  private $shelf;
 
-  function __construct($id, $name, $price, $stock) {
+  function __construct($id, $name, $price, $stock, $provider, $shelf) {
     $this->setId($id);
     $this->setName($name);
     $this->setPrice($price);
     $this->setStock($stock);
+    $this->setProvider($provider);
+    $this->setShelf($shelf);
   }
 
   public function getId(){
     return $this->id;
   }
-  
   public function setId($id){
     $this->id = $id;
   }
+
   public function getName(){
     return $this->name;
   }
-  
   public function setname($name){
     $this->name= $name;
   }
+
   public function getPrice(){
     return $this->price;
   }
-  
   public function setPrice($price){
     $this->price = $price;
   }
+
   public function getStock(){
     return $this->stock;
   }
-  
   public function setStock($stock){
     $this->stock = $stock;
+  }
+
+  public function getProvider(){
+    return $this->provider;
+  }
+  public function setProvider($provider){
+    $this->provider = $provider;
+  }
+
+  public function getShelf(){
+    return $this->shelf;
+  }
+  public function setShelf($shelf){
+    $this->shelf = $shelf;
   }
 }
 ?>
