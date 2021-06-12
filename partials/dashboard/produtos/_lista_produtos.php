98d584c4bb430ff83dc0bb99a65be823faa63198
MarlonVCendron <marlonvcendron@gmail.com>
2021-06-12 15:07:00 -0300

    Implementando lista de produtos

--- partials/dashboard/produtos/_lista_produtos.php
+++ partials/dashboard/produtos/_lista_produtos.php
@@ -1,15 +1,12 @@
 <?php
+require __DIR__ . '/../../../vendor/autoload.php';
 require_once(__DIR__."/../_navbar.php");
 require_once(__DIR__."/_modal_excluir.php");
 use App\entities\Product;
-?>
+use App\dao\ProductDAO;
 
-<?php
-  $produtos = [
-    new Product(1, "Arroz", 5, 312, "312", "312"),
-    new Product(2, "Coiso", 12, "21", "312", "312"),
-    new Product(3, "Outro coiso", 7, "123", "312", "312")
-  ];
+$productDao = new ProductDAO;
+$produtos = $productDao->getAll();
 ?>
 
 <div class="container mt-4">
@@ -29,19 +26,19 @@ use App\entities\Product;
     <tbody>
       <?php foreach ($produtos as $produto) { ?>
       <tr>
-        <th scope="row"><?= $produto->getId() ?></th>
-        <td><?= $produto->getName() ?></td>
-        <td><?= $produto->getPrice() ?></td>
-        <td><?= $produto->getStock() ?></td>
-        <td><?= $produto->getProvider() ?></td>
-        <td><?= $produto->getShelf() ?></td>
+        <th scope="row"><?= $produto["id"] ?></th>
+        <td><?= $produto["name"]?></td>
+        <td><?= $produto["price"]?></td>
+        <td><?= $produto["stock"]?></td>
+        <td><?= $produto["provider"]?></td>
+        <td><?= $produto["shelf"]?></td>
         <td>
-          <?php modalExcluir($produto->getId(), $produto->getName()); ?>
+          <?php modalExcluir($produto["id"], $produto["name"]); ?>
 
-          <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalExcluir<?=$produto->getId()?>">
+          <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalExcluir<?=$produto["id"]?>">
             Excluir
           </button>
-          <a href="http://<?=$_SERVER['HTTP_HOST']?>/dashboard/editar?id=<?=$produto->getId()?>" class="btn btn-info">
+          <a href="http://<?=$_SERVER['HTTP_HOST']?>/dashboard/editar?id=<?=$produto["id"]?>" class="btn btn-info">
             Editar
           </a>
         </td>
