21792578db128770905f7166b16474e52f121fc4
MarlonVCendron <marlonvcendron@gmail.com>
2021-06-13 14:03:28 -0300

    Arrumando indentação

--- src/utils/FlashMessage.php
+++ src/utils/FlashMessage.php
@@ -3,25 +3,24 @@
 namespace App\utils;
 
 class FlashMessage {
-    
-    public const INFO = 'info';
-    public const ERROR = 'danger'; 
-    public const WARNING = 'warning';
-    public const SUCCESS = 'success';
+  
+  public const INFO = 'info';
+  public const ERROR = 'danger'; 
+  public const WARNING = 'warning';
+  public const SUCCESS = 'success';
 
-    public static function setMessage($type, $message) {
-        $_SESSION[$type] = $message;
+  public static function setMessage($type, $message) {
+    $_SESSION[$type] = $message;
+  }
+
+  public static function getMessage($type) {
+    $message = $_SESSION[$type] ?? null;
+    unset($_SESSION[$type]);
+
+    if ($message) {
+      return "<div class='alert alert-{$type}'>{$message}</div>";
+    } else {
+      return;
     }
-
-    public static function getMessage($type) {
-        $message = $_SESSION[$type] ?? null;
-        unset($_SESSION[$type]);
-
-        if ($message) {
-            return "<div class='alert alert-{$type}'>{$message}</div>";
-        } else {
-            return;
-        }
-    }
-
+  }
 }
